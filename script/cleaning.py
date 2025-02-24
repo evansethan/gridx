dfbc9b4ae9fb3f66212158e49e6a9562c9d65419
Ethan Evans <ethanevans@Ethans-MacBook-Air.local>
2025-02-23 21:44:01 -0600

    vis edits

--- script/cleaning.py
+++ script/cleaning.py
@@ -1,79 +1,25 @@
 import pandas as pd 
 import numpy as np
 from datetime import datetime, timedelta
+from info import state_pops_23
 import re
-
-state_populations_2023 = { # use csv census data instead
-    "Alabama": 5074296,
-    "Alaska": 733391,
-    "Arizona": 7359197,
-    "Arkansas": 3045637,
-    "California": 39538223,
-    "Colorado": 5773714,
-    "Connecticut": 3605597,
-    "Delaware": 989948,
-    "Florida": 21944577,
-    "Georgia": 10711908,
-    "Hawaii": 1455271,
-    "Idaho": 1839106,
-    "Illinois": 12812508,
-    "Indiana": 6833037,
-    "Iowa": 3219171,
-    "Kansas": 2937150,
-    "Kentucky": 4505836,
-    "Louisiana": 4624047,
-    "Maine": 1372247,
-    "Maryland": 6185278,
-    "Massachusetts": 7033469,
-    "Michigan": 10077331,
-    "Minnesota": 5706494,
-    "Mississippi": 2961279,
-    "Missouri": 6168187,
-    "Montana": 1104271,
-    "Nebraska": 1961504,
-    "Nevada": 3104614,
-    "New Hampshire": 1377529,
-    "New Jersey": 9288994,
-    "New Mexico": 2117522,
-    "New York": 20201249,
-    "North Carolina": 10551162,
-    "North Dakota": 779094,
-    "Ohio": 11780017,
-    "Oklahoma": 3990443,
-    "Oregon": 4266560,
-    "Pennsylvania": 12964056,
-    "Rhode Island": 1097379,
-    "South Carolina": 5282634,
-    "South Dakota": 909824,
-    "Tennessee": 7051339,
-    "Texas": 31000000,
-    "Utah": 3271616,
-    "Vermont": 643077,
-    "Virginia": 8654542,
-    "Washington": 7738692,
-    "West Virginia": 1793716,
-    "Wisconsin": 5893718,
-    "Wyoming": 576851,
-    "District of Columbia": 689545,
-    "Puerto Rico": 3263584
-}
 
 
 def clean_outages(path):
 
-    df = pd.DataFrame(pd.read_excel("data/outages/2023_Annual_Summary.xls"))
+    df = pd.DataFrame(pd.read_excel(path))
   
     df.columns = df.iloc[0]
     df = df.drop(index=0)
 
-    df = df[df["Number of Customers Affected"] != 0]
-    df = df[df["Number of Customers Affected"] != "0"]
-    df = df[df["Number of Customers Affected"] != "Unknown"]
+    # df = df[df["Number of Customers Affected"] != 0]
+    # df = df[df["Number of Customers Affected"] != "0"]
+    # df = df[df["Number of Customers Affected"] != "Unknown"]
 
     for _, row in df.iterrows():
-        row["Area Affected"] = re.sub(r":.*", "", row["Area Affected"]) # need to handle multiple states...
+        row["Area Affected"] = " ".join(re.findall(r"\b\w+:", row["Area Affected"])) # need to handle multiple states...
 
-        if row["Area Affected"] not in state_populations_2023.keys():
+        if row["Area Affected"] not in state_pops_23.keys():
             if row["Area Affected"] == "LUMA Energy":
                 row["Area Affected"] = "Puerto Rico"
             if row["Area Affected"] == "ISO New England":
@@ -82,7 +28,7 @@ def clean_outages(path):
                 row["Area Affected"] = "Minnesota" #, North Dakota, South Dakota"
             if "Western Area Power" in row["Area Affected"]:
                 row["Area Affected"] = "Montana" #, North Dakota, South Dakota, Nebraska, Iowa, Minnesota"
-
+                
     return df
 
 
@@ -93,18 +39,24 @@ def build_outage_dict(path):
     dic = {}
     for _, row in df.iterrows():
 
-        state = row["Area Affected"]
-        if row["Number of Customers Affected"] == "Unknown":
+        num_affected = row["Number of Customers Affected"]
+        if num_affected == "Unknown":
             continue
-        if state not in dic:
-            dic[state] = int(row["Number of Customers Affected"])
+
+        state = row["Area Affected"]
+        if row["Area Affected"] not in dic:
+            dic[state] = int(num_affected)
         else:
-            dic[state] += int(row["Number of Customers Affected"])
+            dic[state] += int(num_affected)
 
-    del dic["Arkansas, Mississippi"] # get rid of this eventually
-    del dic["Wisconsin, Michigan"]
+    # del dic["Arkansas, Mississippi"] # get rid of this eventually
+    # del dic["Wisconsin, Michigan"]
+    # del dic["Washington, Idaho, Montana"]
+    # del dic['Pacificorp']
 
-    return {x: round((y/state_populations_2023[x])*100, 2) for x,y in dic.items()} # handle multiple states
+    return dic
+
+    return {x: round((y/state_pops_23[x])*100, 2) for x,y in dic.items()} # handle multiple states
 
 
 
--- script/visualization.py
+++ script/visualization.py
@@ -1,27 +1,13 @@
-from urllib.request import urlopen
-import json
 import pandas as pd
 import plotly.express as px
-
+from info import state_abbrev
 
 def show_map():
-    with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-        counties = json.load(response)
 
-
-    df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
-                    dtype={"fips": str})
-
-
-    fig = px.choropleth(df, geojson=counties, locations='fips', color='unemp',
-                            color_continuous_scale="Viridis",
-                            range_color=(0, 12),
-                            scope="usa",
-                            labels={'unemp':'mock data'}
-                            )
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    df = pd.read_csv("data/areas.csv")
+    df['abbrev'] = df['state'].map(state_abbrev)
+    fig = px.choropleth(df, locations="abbrev", locationmode="USA-states", color="area (sq. mi)", range_color=(1000, 150000), scope="usa")
     fig.show()
-
 
 def main():
     show_map()
