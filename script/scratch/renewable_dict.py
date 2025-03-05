9eea5fb420f84842748c35e837a9b0a71fe38975
Ganon Evans <ganon@uchicago.edu>
2025-03-04 20:48:12 -0600

    Adding int

--- script/scratch/renewable_dict.py
+++ script/scratch/renewable_dict.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from pathlib import Path
 
-def get_renewable_production(start_year=2016, end_year=2022):
+def get_renewable_production(start_year:int=2016, end_year:int=2022):
+
     """Takes a range of years and produces a dictionary of dictionaries
     mapping each year to the 50 states's renewable energy % of production."""
     if start_year < 2016 or start_year > 2022:
@@ -12,20 +13,23 @@ def get_renewable_production(start_year=2016, end_year=2022):
         raise KeyError("Please make sure your start and end years are in the correct order.")
     years_to_analyze = list(range(start_year, end_year + 1))
     renewable_path = Path(__file__).parent / "data" / "Renewables" / "prod_btu_re_te.xlsx"
-    state_dict = {}
-    final_dict = {} #needs to be dict of dict for each year then each state to renewables
-    #limit range of 2016 to 2023, can allow subsets
-    #renewable_path = "data/renewables/prod_btu_re_te.xlsx"
+    final_dict = {} 
     renewables_excel = pd.ExcelFile(renewable_path)
     total_renewables_sheet = pd.read_excel(renewables_excel, "Total renewables", header=2)
     total_energy_production_sheet = pd.read_excel(renewables_excel, "Total primary energy", header=2)
     for year in years_to_analyze:
+        all_state_dict = {}
+        renewable_dict = {}
         for index, row in total_renewables_sheet.iterrows():
             state = row["State"]
-            state_dict[state] = {"Total renewables": row[year]}
+            all_state_dict[state] = {"Total renewables": row[year]}
         for index, row in total_energy_production_sheet.iterrows():
             state = row["State"]
-            if state in state_dict:
-                state_dict[state]["Total energy production"] = row[year]
-        for us_state, energy_dict in state_dict.items():
-            energy_dict["Renewable Percent"] = energy_dict["Total renewables"]/energy_dict["Total energy production"]
+            if state in all_state_dict:
+                all_state_dict[state]["Total energy production"] = row[year]
+        for us_state, energy_dict in all_state_dict.items():
+            renewable_dict[us_state] = energy_dict["Total renewables"]/energy_dict["Total energy production"]
+        final_dict[year] = renewable_dict
+    return final_dict
+
+    #renewable_path = "data/renewables/prod_btu_re_te.xlsx" - for proofing a notebook
